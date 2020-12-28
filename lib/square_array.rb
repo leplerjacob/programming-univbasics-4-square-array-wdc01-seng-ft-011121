a27c27f20f252c364d7d156af0eef25027d22f07
Jacob Lepler <lepler.jacob@gmail.com>
2020-12-28 18:06:55 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,5 +2,5 @@ def square_array(array)
   counter = 0
   squared_element_array = Array.new(array.length)
   while array[counter] do
-    squared_element_array[counter] = array[counter] *= array[counter]    
+    squared_element_array[counter] = array[counter] * array[counter]    
 end
